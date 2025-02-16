b254047adf06e46e86698add51967e8d8ee13315
NoahPC <noah.wendt@stud.h-da.de>
2025-02-16 14:32:38 +0100

    Added singletons

---
+++ HaonsTools/Assets/Foo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Haon.Utils;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Foo : MonoSingleton
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+        private void Start()
+        {
+            Debug.Log("Hi");
+            //Testing.DestroyFoo();
+            Testing.Instance.DestroyFoo();
+        }
+    }
+}
---
+++ HaonsTools/Assets/Testing.cs
@@ -0,0 +1,13 @@
+﻿using Haon.Utils;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Testing : Singleton<Testing>
+    {
+        public void DestroyFoo()
+        {
+            Debug.Log("Hello from the singleton realm");
+        }
+    }
+}
